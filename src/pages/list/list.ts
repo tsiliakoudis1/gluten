65d345cc66b66181d68e8943de27d48db50fb6ed
Harris Tsiliakoudis <harris_tsil@hotmail.com>
2017-07-09 22:08:32 +0300

    .

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -5,37 +5,41 @@ import { CategoryPage } from '../../pages/category-page/category-page'
 import { Http } from '@angular/http';
 
 @Component({
-  selector: 'page-list',
-  templateUrl: 'list.html'
+    selector: 'page-list',
+    templateUrl: 'list.html'
 })
 
 
 export class ListPage {
-  public categories;
+    public categories;
 
-  constructor(
+    constructor(
         public navCtrl: NavController,
         public http: Http,
-        ) {
+    ) {
         this.categories = [];
-         this.http.post( 'https://api.cosmicjs.com/v1/authenticate',  { data: { user: 'harris_tsil@hotmail.com', password: 'a29111986' } } ).subscribe((resp) => {
-      let data = resp.json();
+        this.http.post('https://api.cosmicjs.com/v1/authenticate', { data: { user: 'harris_tsil@hotmail.com', password: 'a29111986' } }).subscribe((resp) => {
+            let data = resp.json();
 
-      console.log(data);
-    });
-    
-        
+            console.log(data);
+        });
+
+
         Cosmic.getObjectType({
             bucket: {
-                slug: 'gluten'
+                slug: 'gluten',
+                read_key: '6CIZzNh8tp2w1wEOgNhFukFwtQUw8MCaPaKsps4oUfNwW4l4y2',
             }
         }, {
-            type_slug: 'categories'
-        }, (err, res) => {
-            this.categories = res.objects.all;
-        });
+                type_slug: 'categories'
+            }, (err, res) => {
+                this.categories = res.objects.all;
+            });
     }
     navToCategory(category) {
-        this.navCtrl.push(CategoryPage, { title: category.title});
+        this.navCtrl.push(CategoryPage, { title: category.title });
     }
+
+
 }
+
